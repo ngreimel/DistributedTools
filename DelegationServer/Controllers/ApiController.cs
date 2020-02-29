38c99397e816b99970e92f1f61d7aa4cb1dc15ba
Allan Stewart <stewart.allan@gmail.com>
2020-02-28 23:12:16 -0700

    Always return some json, and map votes correctly

--- DelegationServer/Controllers/ApiController.cs
+++ DelegationServer/Controllers/ApiController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using DelegationServer.Domain;
 using DelegationServer.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -17,49 +18,67 @@ namespace DelegationServer.Controllers
         [HttpGet("state")]
         public IActionResult GetState()
         {
-            return Ok(delegationSession.GetData());
+            var data = delegationSession.GetData();
+            return Ok(new
+            {
+                data.Users,
+                Items = data.Items.Select(i => new
+                {
+                    i.ItemId,
+                    i.Description,
+                    i.IsVisible,
+                    Votes = i.Votes
+                        .Select(v => new
+                        {
+                            userId = v.Key,
+                            vote = v.Value
+                        })
+                        .ToList()
+                }),
+                data.CurrentItemId
+            });
         }
 
         [HttpPost("register")]
         public IActionResult RegisterVoter([FromBody] UserRegistrationRequest request)
         {
-            delegationSession.RegisterUser(request.Name, UserType.Voter);
-            return Ok();
+            var userId = delegationSession.RegisterUser(request.Name, UserType.Voter);
+            return Ok(new { userId });
         }
         
         [HttpPost("register-admin")]
         public IActionResult RegisterAdmin([FromBody] UserRegistrationRequest request)
         {
-            delegationSession.RegisterUser(request.Name, UserType.Admin);
-            return Ok();
+            var userId = delegationSession.RegisterUser(request.Name, UserType.Admin);
+            return Ok(new { userId });
         }
         
         [HttpPost("add-item")]
         public IActionResult AddItem([FromBody] AddItemRequest request)
         {
             delegationSession.AddItem(request.Description);
-            return Ok();
+            return Ok(new {});
         }
         
         [HttpPost("set-current-item")]
         public IActionResult SetCurrentItem([FromBody] SetCurrentItemRequest request)
         {
             delegationSession.SetCurrentItem(request.ItemId, request.UserId);
-            return Ok();
+            return Ok(new {});
         }
         
         [HttpPost("vote")]
         public IActionResult Vote([FromBody] VoteRequest request)
         {
             delegationSession.Vote(request.ItemId, request.UserId, request.Vote);
-            return Ok();
+            return Ok(new {});
         }
         
         [HttpPost("make-visible")]
         public IActionResult MakeVisible([FromBody] MakeVisibleRequest request)
         {
             delegationSession.MakeVisible(request.ItemId, request.UserId);
-            return Ok();
+            return Ok(new {});
         }
     }
 }
