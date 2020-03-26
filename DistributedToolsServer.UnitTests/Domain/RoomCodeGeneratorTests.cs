f44e615afc48ca84c9bf1099bcdd97cc1efc1b3f
Allan Stewart <allans@emmersionlearning.com>
2020-03-26 12:47:51 -0600

    Add room code generator

---
+++ DistributedToolsServer.UnitTests/Domain/RoomCodeGeneratorTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using DistributedToolsServer.Domain;
+using NUnit.Framework;
+
+namespace DistributedToolsServer.UnitTests.Domain
+{
+    public class RoomCodeGeneratorTests
+    {
+        private RoomCodeGenerator classUnderTest;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            classUnderTest = new RoomCodeGenerator();
+        }
+        
+        [Test]
+        public void When_generating_a_code()
+        {
+            var code = classUnderTest.Generate();
+            Assert.That(code.Length, Is.EqualTo(4));
+            var pattern = new Regex("[2-9ABCFGHJKPRSTWXYZ]{4}");
+            Assert.That(pattern.IsMatch(code), Is.True);
+        }
+        
+        [Test]
+        public void When_generating_a_code_it_is_different_from_the_last_code()
+        {
+            var code1 = classUnderTest.Generate();
+            var code2 = classUnderTest.Generate();
+            Assert.That(code1, Is.Not.EqualTo(code2));
+        }
+    }
+}
---
+++ DistributedToolsServer/Domain/RoomCodeGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace DistributedToolsServer.Domain
+{
+    public class RoomCodeGenerator
+    {
+        private static char[] validCharacters = "23456789ABCFGHJKPRSTWXYZ".ToCharArray();
+        private static Random random = new Random();
+        
+        public string Generate()
+        {
+            var characters = Enumerable.Range(0, 4).Select(_ => validCharacters[random.Next(0, validCharacters.Length)]);
+            return string.Join("", characters);
+        }
+    }
+}
