057a5f2026d55763921a4c1f68369480949f4f85
Allan Stewart <allans@emmersionlearning.com>
2020-04-01 07:25:29 -0600

    Formatting

--- DistributedToolsServer/Controllers/DecisionDelegationPokerController.cs
+++ DistributedToolsServer/Controllers/DecisionDelegationPokerController.cs
@@ -14,13 +14,13 @@ namespace DistributedToolsServer.Controllers
         {
             this.roomSessionRepository = roomSessionRepository;
         }
-        
+
         [Route("")]
         public IActionResult Index(string roomCode)
         {
             return View("Index", roomCode);
         }
-        
+
         private IDecisionDelegationSession getSession(string roomCode)
         {
             return roomSessionRepository.GetDecisionDelegationSession(roomCode);
@@ -62,35 +62,35 @@ namespace DistributedToolsServer.Controllers
             var userId = getSession(roomCode).RegisterUser(request.Name, UserType.Voter);
             return Ok(new { userId });
         }
-        
+
         [HttpPost("register-admin")]
         public IActionResult RegisterAdmin(string roomCode, [FromBody] UserRegistrationRequest request)
         {
             var userId = getSession(roomCode).RegisterUser(request.Name, UserType.Admin);
             return Ok(new { userId });
         }
-        
+
         [HttpPost("add-item")]
         public IActionResult AddItem(string roomCode, [FromBody] AddItemRequest request)
         {
             getSession(roomCode).AddItem(request.Description);
             return Ok(new {});
         }
-        
+
         [HttpPost("set-current-item")]
         public IActionResult SetCurrentItem(string roomCode, [FromBody] SetCurrentItemRequest request)
         {
             getSession(roomCode).SetCurrentItem(request.ItemId, request.UserId);
             return Ok(new {});
         }
-        
+
         [HttpPost("vote")]
         public IActionResult Vote(string roomCode, [FromBody] VoteRequest request)
         {
             getSession(roomCode).Vote(request.ItemId, request.UserId, request.Vote);
             return Ok(new {});
         }
-        
+
         [HttpPost("make-visible")]
         public IActionResult MakeVisible(string roomCode, [FromBody] MakeVisibleRequest request)
         {
